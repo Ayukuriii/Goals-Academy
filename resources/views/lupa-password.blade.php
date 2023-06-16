bbf0de6845c22a5f8984107a7a40bd22332e9a21
Hilda Zaqya <hildazaqyaelnaz@gmail.com>
2023-06-16 14:22:57 +0700

    halaman lupa password

--- resources/views/lupa-password.blade.php
+++ resources/views/lupa-password.blade.php
@@ -14,7 +14,7 @@
     <title>Goals Academy | {{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link href="style-password.css" rel="stylesheet" /> <!-- // style login -->
+    <link href="{{ asset('css/style-password.css') }}" rel="stylesheet" /> <!-- // style login -->
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -50,7 +50,7 @@
                 </form>
             </div>
             <div class="d-flex justify-content-center align-items-center">
-                <img src="./assets/icons/company.png" width="15px" height="15px"/>
+                <img src="image/assets/icons/company.png" width="15px" height="15px"/>
                 <p class="text-center fw-bold mt-3 ms-2" style="color: #ffff; font-size:12px;">
                     2023 Goals Academy
                 </p>
