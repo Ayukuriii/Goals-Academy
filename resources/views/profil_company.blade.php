42cf9352cb16f07d651dc05ce79c425c829cdcd1
Akhmad Roziqin <roziqinakhmad14juli@gmail.com>
2023-07-05 10:36:33 +0700

    perbaikan page profil conpany dan karir

--- resources/views/profil_company.blade.php
+++ resources/views/profil_company.blade.php
@@ -2,25 +2,31 @@
 
 @section('container')
 <!-- Isi Page -->
-<section id="profil">
-    <div class="profil-image container justify-content-center align-items-center">
-        <img class="img-fluid logo-goals-photo" alt="" src="image/assets/icons/LOGO PRIMARY_ORANGE.png" width="350px" height="290px" />
-    </div>
-
-    <div class="text-profile">
-        <div class="text-1">
-    <p style="font-weight: bold; color: #4F3F85;">Apa itu Goals Academy?</p>
-            <p style="color:#363434 ;">Platform yang berfokus pada bimbingan skripsi secara hybrid, bisa online maupun offline,  
-                yang menyediakan berbagai macam program bimbingan yang membantu mahasiswa dalam  
-                mengerjakan skripsi dari awal hingga lulus, tentunya dengan bimbingan langsung oleh tutor 
-                yang berpengalaman dan ahli di bidangnya.</p>
-            <br>
-            <p style="font-weight: bold; color: #4F3F85;">Visi Kami</p>
-            <p style="color:#363434 ;">Berkomitmen dalam mentransformasi tenaga pendidik profesional dan pelajar sebagai pemimpin di 
-                 masa depan dengan mengedepankan kemajuan dan efektifitas di bidang riset,  
-                 pendidikan dan keilmuan berbasis inovasi dan teknologi.</p>
+<section id="profil" class="pt-5">
+    <div class="profil-image container justify-content-center align-items-center mt-5">
+        <div class="row justify-content-center">
+            <img class="img-fluid logo-goals-photo col-5 col-md-3" alt="" src="image/assets/icons/LOGO PRIMARY_ORANGE.png"/>
+        </div>
+        <div class="text-profile mt-4">
+            <div class="text-1">
+                <p style="font-weight: bold; color: #4F3F85;">Apa itu Goals Academy?</p>
+                <p style="color:#363434 ;">
+                    Platform yang berfokus pada bimbingan skripsi secara hybrid, bisa online maupun offline,
+                    yang menyediakan berbagai macam program bimbingan yang membantu mahasiswa dalam
+                    mengerjakan skripsi dari awal hingga lulus, tentunya dengan bimbingan langsung oleh tutor
+                    yang berpengalaman dan ahli di bidangnya.
+                </p>
+                <br>
+                <p style="font-weight: bold; color: #4F3F85;">Visi Kami</p>
+                <p style="color:#363434 ;">
+                    Berkomitmen dalam mentransformasi tenaga pendidik profesional dan pelajar sebagai pemimpin
+                    di masa depan dengan mengedepankan kemajuan dan efektifitas di bidang riset, pendidikan dan
+                    keilmuan berbasis inovasi dan teknologi.
+                </p>
+            </div>
         </div>
     </div>
+
     </div>
 </section>
 <!-- Last Page -->
