0709ad4f33757bd768ec2f1c63ff789cf9d1db98
Akhmad Roziqin <roziqinakhmad14juli@gmail.com>
2023-07-10 09:11:11 +0700

    minor change

--- resources/views/dashboard/admin/bimbingan/detail-bimbingan.blade.php
+++ resources/views/dashboard/admin/bimbingan/detail-bimbingan.blade.php
@@ -23,7 +23,7 @@
                             </tr>
                             <tr>
                                 <td>TUTOR</td>
-                                <td class="fw-bold small">Kak {{ $data->tutor->user->name }}</td>
+                                <td class="fw-bold small">Kak {{ $data->tutor->user->name ?? 'Kosong' }}</td>
                             </tr>
                             <tr>
                                 <td>JADWAL</td>
